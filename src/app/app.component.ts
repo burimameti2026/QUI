ef5dcfceb0b100e1cecef4d48786502779b9937e
burimameti2026 <burimameti44@gmail.com>
2026-09-12 21:44:41 +0200

    fix(renova): remove duplicate global i18n observer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AdminStaticI18nDirective } from './core/admin-static-i18n.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminStaticI18nDirective],
-  template: '<div class="app-i18n-root" qaiAdminStaticI18n><router-outlet /></div>'
+  imports: [RouterOutlet],
+  template: '<router-outlet />'
 })
 export class AppComponent {}
