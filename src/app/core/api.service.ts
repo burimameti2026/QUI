f7db5e2b6435900f6206a644cdb75c4bdaa4c5d2
burimameti2026 <burimameti44@gmail.com>
2026-09-14 23:59:54 +0200

    fix(http): bound API request lifetime

--- src/app/core/api.service.ts
+++ src/app/core/api.service.ts
@@ -1,2 +1,26 @@
-import { Injectable } from '@angular/core'; import { HttpClient } from '@angular/common/http'; import { Observable } from 'rxjs';
-@Injectable({providedIn:'root'}) export class ApiService { constructor(private http:HttpClient){} get<T>(u:string):Observable<T>{return this.http.get<T>('/api/'+u)} post<T>(u:string,b:any):Observable<T>{return this.http.post<T>('/api/'+u,b)} put<T>(u:string,b:any):Observable<T>{return this.http.put<T>('/api/'+u,b)} delete<T>(u:string):Observable<T>{return this.http.delete<T>('/api/'+u)} }
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, timeout } from 'rxjs';
+
+@Injectable({providedIn:'root'})
+export class ApiService {
+  private readonly requestTimeoutMs = 15000;
+
+  constructor(private http: HttpClient) {}
+
+  get<T>(u: string): Observable<T> {
+    return this.http.get<T>('/api/' + u, { timeout: this.requestTimeoutMs });
+  }
+
+  post<T>(u: string, b: any): Observable<T> {
+    return this.http.post<T>('/api/' + u, b, { timeout: this.requestTimeoutMs });
+  }
+
+  put<T>(u: string, b: any): Observable<T> {
+    return this.http.put<T>('/api/' + u, b, { timeout: this.requestTimeoutMs });
+  }
+
+  delete<T>(u: string): Observable<T> {
+    return this.http.delete<T>('/api/' + u, { timeout: this.requestTimeoutMs });
+  }
+}
