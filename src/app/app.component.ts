5020e11be4256c9cb670112da37a0a92dcb9982d
burimameti2026 <burimameti44@gmail.com>
2026-09-18 23:59:00 +0200

    load tenant branding at app startup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BrandThemeService } from './core/brand-theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   template: '<router-outlet />'
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private brandTheme: BrandThemeService) {}
+
+  ngOnInit() {
+    this.brandTheme.load().subscribe();
+  }
+}
