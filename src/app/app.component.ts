0e0e90f197ac78af9ea7491d640778339a4f245a
burimameti2026 <burimameti44@gmail.com>
2026-09-21 15:15:18 +0200

    feat: enable shared multilingual translation across all routed components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BrandThemeService } from './core/brand-theme.service';
 import { WhiteLabelConfigService } from './core/white-label-config.service';
+import { AdminStaticI18nDirective } from './core/admin-static-i18n.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: '<router-outlet />'
+  imports: [RouterOutlet, AdminStaticI18nDirective],
+  template: '<div class="app-i18n-shell" qaiAdminStaticI18n><router-outlet /></div>'
 })
 export class AppComponent implements OnInit {
   constructor(private brandTheme: BrandThemeService, private whiteLabelConfig: WhiteLabelConfigService) {}
