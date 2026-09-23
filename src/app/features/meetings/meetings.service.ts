fe12496c5a7df6fac56993ae0e59a2e09de46ef5
burimameti2026 <burimameti44@gmail.com>
2026-09-23 15:53:15 +0200

    Align meetings UI service with backend endpoints

--- src/app/features/meetings/meetings.service.ts
+++ src/app/features/meetings/meetings.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "../../core/api.service";
+
 @Injectable({ providedIn: "root" })
 export class MeetingsService {
   constructor(private api: ApiService) {}
+
   list() {
     return this.api.get<any[]>("meetings");
   }
+
   types() {
     return this.api.get<any[]>("meetings/types");
   }
+
   create(x: any) {
     return this.api.post<any>("meetings", x);
-  }
-  update(id: string, x: any) {
-    return this.api.put<any>(`meetings/${id}`, x);
-  }
-  remove(id: string) {
-    return this.api.delete<void>(`meetings/${id}`);
   }
 }
