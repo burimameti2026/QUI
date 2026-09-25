968b3547543a32fb44bec7a7b6a21da6564857c2
burimameti2026 <burimameti44@gmail.com>
2026-09-26 00:23:33 +0200

    feat: provision campaign containers from industry packs

--- src/app/features/industry-packs/industry-packs.service.ts
+++ src/app/features/industry-packs/industry-packs.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiService } from '../../core/api.service';
-@Injectable({providedIn:'root'})
-export class IndustryPacksService{
-  constructor(private api:ApiService){}
-  list<T=any>():Observable<T>{return this.api.get<T>('industry-packs')}
-  install<T=any>(id:string){return this.api.post<T>(`industry-packs/${id}/install`,{})}
+
+@Injectable({ providedIn: 'root' })
+export class IndustryPacksService {
+  constructor(private api: ApiService) {}
+
+  list<T = any>(): Observable<T> {
+    return this.api.get<T>('industry-packs');
+  }
+
+  provision<T = any>(id: string): Observable<T> {
+    return this.api.post<T>(`industry-packs/${id}/provision`, {});
+  }
+
+  install<T = any>(id: string): Observable<T> {
+    return this.provision<T>(id);
+  }
 }
