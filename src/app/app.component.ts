732cdea9a3293905b55da07c57ecc62588b84028
burimameti2026 <burimameti44@gmail.com>
2026-09-26 19:40:18 +0200

    feat(ui): mount global API error banner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { BrandThemeService } from './core/brand-theme.service';
 import { WhiteLabelConfigService } from './core/white-label-config.service';
 import { AdminStaticI18nDirective } from './core/admin-static-i18n.directive';
+import { ApiErrorBannerComponent } from './core/api-error-banner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AdminStaticI18nDirective],
-  template: '<div class="app-i18n-shell" qaiAdminStaticI18n><router-outlet /></div>'
+  imports: [RouterOutlet, AdminStaticI18nDirective, ApiErrorBannerComponent],
+  template: `
+    <div class="app-i18n-shell" qaiAdminStaticI18n>
+      <router-outlet />
+      <qai-api-error-banner />
+    </div>
+  `
 })
 export class AppComponent implements OnInit {
   constructor(private brandTheme: BrandThemeService, private whiteLabelConfig: WhiteLabelConfigService) {}
