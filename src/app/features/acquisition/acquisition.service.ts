329f862502f23e26682fe2621718f349c3e6bd9a
burimameti2026 <burimameti44@gmail.com>
2026-09-26 15:32:44 +0200

    Expose ICP campaign and prospecting APIs in acquisition service

--- src/app/features/acquisition/acquisition.service.ts
+++ src/app/features/acquisition/acquisition.service.ts
@@ -13,6 +13,11 @@ export class AcquisitionService {
   stopCampaign(id: string) { return this.api.post<any>(`acquisition/campaigns/${id}/stop`, {}); }
   deleteCampaign(id: string) { return this.api.delete<any>(`acquisition/campaigns/${id}`); }
   saveCampaignPlan(id: string, planJson: string) { return this.api.put<any>(`acquisition/campaigns/${id}/plan`, { planJson }); }
+  icps() { return this.api.get<any[]>(`acquisition/icp`); }
+  saveIcp(input: any) { return this.api.post<any>(`acquisition/icp`, input); }
+  createCampaign(input: any) { return this.api.post<any>(`acquisition/campaigns`, input); }
+  prospects(minimumScore = 0) { return this.api.get<any[]>(`acquisition/prospects?minimumScore=${minimumScore}`); }
+  targetLists() { return this.api.get<any[]>(`acquisition/target-lists`); }
 
   messages() { return this.api.get<any[]>("acquisition/messages"); }
   approve(id: string) { return this.api.post<any>(`email-operations/messages/${id}/approve`, {}); }
